2f1faca22eb643bdf0b27004868dd9eb775938f1
shokkun0407 <shoki1310@gmail.com>
2024-08-16 15:48:17 +0900

    addnewfiles

--- Assets/MyScripts/PlayerFollower.cs
+++ Assets/MyScripts/PlayerFollower.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class PlayerFollower : MonoBehaviour
 {
-
-    public GameObject player; 
+    public GameObject player;
+    Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = this.transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        this.transform.position = player.transform.position;
+        this.transform.position = player.transform.position + offset;
     }
 }
