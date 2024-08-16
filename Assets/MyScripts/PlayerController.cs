451df0ca0331794fff75cc1ec94b956c47b6ddeb
shokkun0407 <shoki1310@gmail.com>
2024-08-16 18:04:22 +0900

    appdate files

--- Assets/MyScripts/PlayerController.cs
+++ Assets/MyScripts/PlayerController.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    int count;
+    public Text countText;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -22,4 +27,18 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(moveH, 0, moveV);
         rb.AddForce(move * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        count = count + 1;
+                SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text ="ゲット数：" +  count.ToString();
+
+    }
 }
+
